867049955db9841be73deae113e8ad29886659f8
Luca Reccia <lucareccia@hotmail.it>
2019-11-17 18:06:20 +0100

    added id for blocks

--- src/compressione/Dictionary.java
+++ src/compressione/Dictionary.java
@@ -10,11 +10,13 @@ import java.io.IOException;
 public class Dictionary{
 
     private Map<String, List<Long>> dict;
+    private List<String> blocks;
     private int c;
 
     public Dictionary(int c){
         this.c = c;
         this.dict = new HashMap<String, List<Long>>();
+        this.blocks = new ArrayList<String>();
     }
 
     public void addFile(String filename){
@@ -25,9 +27,14 @@ public class Dictionary{
             int readBytes = 0;
             while((readBytes  = is.read(b)) != -1){
                 String currentBlock = new String(Arrays.copyOfRange(b, 0, readBytes));
-                List<Long> currentList = this.dict.getOrDefault(currentBlock, new ArrayList<Long>());
 
-                //TODO: all posto di "asd" ci deve andare qualcosa che mi permetta di avere un puntatore a quel punto del file
+                List<Long> currentList = this.dict.get(currentBlock);
+                if(currentList == null){
+                    //in questo caso è un blocco nuovo e lo devo aggiungere alla array di blocks
+                    this.blocks.add(currentBlock);
+                    currentList = new ArrayList<Long>();
+                }
+
                 currentList.add(is.getFilePointer() - readBytes);
                 this.dict.put(currentBlock, currentList);
             }
@@ -36,6 +43,17 @@ public class Dictionary{
         }catch(IOException ioe){
             System.out.println("Error "+ioe.getMessage());
         }
+    }
+
+    public String getBlockFromIf(int id){
+        return this.blocks.get(id);
+    }
+
+    public int getIdFromBlock(String block){
+        for(int i=0; i<this.blocks.size(); i++)
+            if(this.blocks.get(i).equals(block))
+                return i;
+        return -1;
     }
 
     @Override
@@ -49,6 +67,12 @@ public class Dictionary{
             }
             out+="]\n\n";
         }
+
+        out+="Blocks: [";
+        for(String block: this.blocks){
+            out+= " "+block+" ";
+        }
+        out+="]\n\n";
         return out;
     }
 }
